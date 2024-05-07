9fe10e994830c8bb520a5063da97ca11206296e2
jdeksnys <jonasdeksnys@gamil.com>
2024-05-07 21:49:33 +0300

    test-pass

--- test/untestable1.test.mjs
+++ test/untestable1.test.mjs
@@ -3,8 +3,9 @@ import { expect } from "chai";
 import { daysUntilChristmas } from "../src/untestable1.mjs";
 
 describe("Untestable 1: days until Christmas", () => {
-  test("todo", () => {
-    // TODO: write proper tests
-    expect(daysUntilChristmas()).to.be.a("number");
+  test("0 until xmas", () => {
+    let date0 = new Date("2022-12-25T00:00:00");
+
+    expect(daysUntilChristmas(date0)).to.equal(0);
   });
 });
