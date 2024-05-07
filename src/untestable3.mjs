6c3808adcd23b61828b86a6386ee6022c547ea8c
jdeksnys <jonasdeksnys@gamil.com>
2024-05-07 22:32:29 +0300

    test-pass

--- src/untestable3.mjs
+++ src/untestable3.mjs
@@ -1,12 +1,18 @@
 import { readFile } from "node:fs/promises";
 import { parse } from "csv-parse/sync";
 
-export async function parsePeopleCsv(filePath) {
-  const csvData = await readFile(filePath, { encoding: "utf8" });
+export async function readFile(filePath){
+  let res = await readFile(filePath, { encoding: "utf8" });
+  return await res;
+}
+
+export function parsePeopleCsv(csvData) {
+  //need actual test file on machine to run test. Just pass csvData as function parameter.
   const records = parse(csvData, {
     skip_empty_lines: true,
     trim: true,
   });
+
   return records.map(([firstName, lastName, age, gender]) => {
     const person = {
       firstName,
--- test/untestable3.test.mjs
+++ test/untestable3.test.mjs
@@ -1,6 +1,6 @@
 import { describe, test } from "vitest";
 import { expect } from "chai";
-import { parsePeopleCsv } from "../src/untestable3.mjs";
+import { parsePeopleCsv, readFile } from "../src/untestable3.mjs";
 
 // example input:
 // Loid,Forger,,Male
@@ -8,10 +8,7 @@ import { parsePeopleCsv } from "../src/untestable3.mjs";
 // Yor,Forger,27,Female
 
 describe("Untestable 3: CSV file parsing", () => {
-  test("todo", async () => {
-    // TODO: write proper tests
-    try {
-      expect(await parsePeopleCsv("people.csv")).to.deep.equal([]);
-    } catch (e) {}
+  test("readFile() reads ok", async () => {
+    expect(await readFile("./test/dummy_file.txt")).to.equal("abc123\n");
   });
 });
