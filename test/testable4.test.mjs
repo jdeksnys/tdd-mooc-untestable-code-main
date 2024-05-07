1f936db02a3bfef5afae6f669fec6b7769537adf
jdeksnys <jonasdeksnys@gamil.com>
2024-05-08 00:55:12 +0300

    test-pass

--- test/testable4.test.mjs
+++ test/testable4.test.mjs
@@ -26,7 +26,27 @@ describe("Globals and singletons: enterprise application", () => {
     const user1 = await users.getById(userId);
 
     expect(user1.passwordHash).to.not.equal(user0.passwordHash);
-
     expect(hasher.verifyPassword(user1.passwordHash, "pass1")).to.be.true;
+  });
+
+  test("change the password, check hashes", async () => {
+    let error;
+    const user0 = {
+      userId,
+      passwordHash: hasher.hashPassword("pass0")
+    };
+    await users.save(user0);
+
+    try{
+      await service.changePassword(userId, "pass0_wrong", "pass1");
+    } catch(err){
+      error = err;
+    }
+
+    expect(error).to.deep.equal(new Error("wrong old password"));
+    
+    const user1 = await users.getById(userId);
+    expect(user1.passwordHash).to.equal(user0.passwordHash);
+    expect(hasher.verifyPassword(user1.passwordHash, "pass0")).to.be.true;
   });
 });
