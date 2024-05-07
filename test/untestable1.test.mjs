01b8b5c044c9dad8d3e0bdb5ac4022d743c43e7f
jdeksnys <jonasdeksnys@gamil.com>
2024-05-07 21:52:42 +0300

    test-pass

--- test/untestable1.test.mjs
+++ test/untestable1.test.mjs
@@ -4,8 +4,13 @@ import { daysUntilChristmas } from "../src/untestable1.mjs";
 
 describe("Untestable 1: days until Christmas", () => {
   test("0 until xmas", () => {
-    let date0 = new Date("2022-12-25T00:00:00");
+    let date0 = new Date("2024-12-25T00:00:00");
+    let date1 = new Date("2024-12-25T23:59:59");
+    let date2 = new Date("2024-12-25T12:00:00");
 
     expect(daysUntilChristmas(date0)).to.equal(0);
+    expect(daysUntilChristmas(date1)).to.equal(0);
+    expect(daysUntilChristmas(date2)).to.equal(0);
   });
+
 });
